e87c1eb9139f0384633b13eb8c3733a364f2feee
Dũng TT <dungtienthe1920@gmail.com>
2025-12-06 14:03:53 +0700

    feat(domain): implement Result Pattern

---
+++ src/ERP.Domain/Shared/ResultT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Domain.Shared
+{
+    public class Result<TValue> : Result
+    {
+        private readonly TValue? _value;
+
+        protected internal Result(TValue? value, bool isSuccess, Error error)
+            : base(isSuccess, error) =>
+            _value = value;
+
+        public TValue Value => IsSuccess
+            ? _value!
+            : throw new InvalidOperationException("Không thể lấy giá trị khi kết quả là lỗi.");
+
+        public static implicit operator Result<TValue>(TValue? value) => Create(value);
+    }
+}
