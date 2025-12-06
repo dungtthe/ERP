bda977655677e98fec4fe93ed44f9089963817e9
Dũng TT <dungtienthe1920@gmail.com>
2025-12-06 13:50:53 +0700

    feat(domain): develop primitives for DDD (Entity, ValueObject, AggregateRoot, DomainEvent)

---
+++ src/ERP.Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Domain.Primitives
+{
+    public abstract class AggregateRoot : Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        protected AggregateRoot(Guid id)
+            : base(id)
+        {
+        }
+
+        protected AggregateRoot()
+        {
+        }
+
+        public IReadOnlyCollection<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
+
+        public void ClearDomainEvents() => _domainEvents.Clear();
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent) =>
+            _domainEvents.Add(domainEvent);
+    }
+}
