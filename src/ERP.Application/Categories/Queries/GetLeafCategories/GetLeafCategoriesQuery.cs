114cbb07fc46dd39eb9f4c2be6cb83024357977a
Vu Binh <22520129@gm.uit.edu.vn>
2025-12-15 21:57:23 +0700

    feat(category): add API to get leaf categories

---
+++ src/ERP.Application/Categories/Queries/GetLeafCategories/GetLeafCategoriesQuery.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ERP.Application.Abstractions.Messaging;
+
+namespace ERP.Application.Categories.Queries.GetLeafCategories
+{
+    public class GetLeafCategoriesQuery : IQuery<List<LeafCategoryResponse>>
+    {
+        
+    }
+}
