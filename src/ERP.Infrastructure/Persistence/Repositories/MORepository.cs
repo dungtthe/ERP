6d6a70609e65ced97480cb4465053b9fb658aa43
Hồ Tiến Vũ Bình <146900914+VuBinhVL@users.noreply.github.com>
2026-01-02 23:05:59 +0700

    feat(manufacturing-order): update DoneMO command to set QuantityProduced when marking manufacturing order as done

--- src/ERP.Infrastructure/Persistence/Repositories/MORepository.cs
+++ src/ERP.Infrastructure/Persistence/Repositories/MORepository.cs
@@ -6,56 +6,58 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ERP.Infrastructure.Persistence.Repositories
 {
-    public class MORepository : IMORepository
-    {
-        private readonly AppDbContext _context;
-        private readonly IReadAppDbContext _readContext;
-        public MORepository(AppDbContext context, IReadAppDbContext readContext)
-        {
-            _context = context;
-            _readContext = readContext;
-        }
+	public class MORepository : IMORepository
+	{
+		private readonly AppDbContext _context;
+		private readonly IReadAppDbContext _readContext;
 
-        public async Task AddAsync(ManufacturingOrder manufacturingOrder, CancellationToken cancellationToken)
-        {
-            await _context.ManufacturingOrders.AddAsync(manufacturingOrder, cancellationToken);
-        }
+		public MORepository(AppDbContext context, IReadAppDbContext readContext)
+		{
+			_context = context;
+			_readContext = readContext;
+		}
 
-        public async Task CancelAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
-        {
-            var manufacturingOrder = await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId);
-            if (manufacturingOrder != null)
-            {
-                manufacturingOrder.ManufacturingOrderStatus = ManufacturingOrderStatus.Cancelled;
-            }
-        }
+		public async Task AddAsync(ManufacturingOrder manufacturingOrder, CancellationToken cancellationToken)
+		{
+			await _context.ManufacturingOrders.AddAsync(manufacturingOrder, cancellationToken);
+		}
 
-        public async Task ConfirmAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
-        {
-            var manufacturingOrder = await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId);
-            if (manufacturingOrder != null)
-            {
-                manufacturingOrder.ManufacturingOrderStatus = ManufacturingOrderStatus.Confirmed;
-            }
-        }
+		public async Task CancelAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
+		{
+			var manufacturingOrder = await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId);
+			if (manufacturingOrder != null)
+			{
+				manufacturingOrder.ManufacturingOrderStatus = ManufacturingOrderStatus.Cancelled;
+			}
+		}
 
-        public async Task DoneAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
-        {
-            var manufacturingOrder = await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId);
-            if (manufacturingOrder != null)
-            {
-                manufacturingOrder.ManufacturingOrderStatus = ManufacturingOrderStatus.Done;
-            }
-        }
+		public async Task ConfirmAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
+		{
+			var manufacturingOrder = await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId);
+			if (manufacturingOrder != null)
+			{
+				manufacturingOrder.ManufacturingOrderStatus = ManufacturingOrderStatus.Confirmed;
+			}
+		}
 
-        public async Task<ManufacturingOrder> GetByIdAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
-        {
-            return await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId, cancellationToken) ?? null!;
-        }
+		public async Task DoneAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
+		{
+			var manufacturingOrder = await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId);
+			if (manufacturingOrder != null)
+			{
+				manufacturingOrder.ManufacturingOrderStatus = ManufacturingOrderStatus.Done;
+				manufacturingOrder.QuantityProduced = manufacturingOrder.QuantityToProduce;
+			}
+		}
 
-        public async Task<bool> IsManufacturingOrderExistsAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
-        {
-            return await _readContext.ManufacturingOrders.AnyAsync(x => x.Id == manufacturingOrderId, cancellationToken);
-        }
-    }
+		public async Task<ManufacturingOrder> GetByIdAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
+		{
+			return await _context.ManufacturingOrders.FirstOrDefaultAsync(x => x.Id == manufacturingOrderId, cancellationToken) ?? null!;
+		}
+
+		public async Task<bool> IsManufacturingOrderExistsAsync(Guid manufacturingOrderId, CancellationToken cancellationToken)
+		{
+			return await _readContext.ManufacturingOrders.AnyAsync(x => x.Id == manufacturingOrderId, cancellationToken);
+		}
+	}
 }
